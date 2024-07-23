83be0bbc366ab99a042e18680ed9ac7874c66a1e
Santino Castagno <santinoo.rn@gmail.com>
2024-07-23 16:31:37 -0300

    feat: get-translations/ & get-translation/ beginned

--- backend/api/handlers/translation.go
+++ backend/api/handlers/translation.go
@@ -16,13 +16,12 @@ type Env struct {
 }
 
 func (e *Env) GenerateTranslation(w http.ResponseWriter, r *http.Request) {
-	var translation models.Translation
-
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
+	var translation models.Translation
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&translation)
 	if err != nil {
@@ -44,17 +43,63 @@ func (e *Env) GenerateTranslation(w http.ResponseWriter, r *http.Request) {
 }
 
 func (e *Env) GetTranslations(w http.ResponseWriter, r *http.Request) {
-	var translations []models.Translation
 	if r.Method != http.MethodGet {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-	//TODO: Continue from here
+	rows, err := e.Db.Query("SELECT id, source, destination FROM translations")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
 
-	// generate response and return answer
-	response := map[string]string{"msg": "New translation has been added to database."}
+	var translations []models.Translation
+	for rows.Next() {
+		var translation models.Translation
+		err := rows.Scan(&translation.ID, &translation.Source, &translation.Destionation)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		translations = append(translations, translation)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(translations)
+}
+
+func (e *Env) GetTranslation(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	source := r.URL.Query().Get("source")
+	if source == "" {
+		http.Error(w, "Missing 'source' parameter", http.StatusBadRequest)
+		return
+	}
+
+	var translation models.Translation
+	err := e.Db.QueryRow("SELECT id, source, destination FROM translations WHERE source = $1", translation.Source).Scan(&translation.ID, &translation.Source, &translation.Destionation)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Translation not found", http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(translation)
 }
--- backend/models/translation.go
+++ backend/models/translation.go
@@ -3,5 +3,5 @@ package models
 type Translation struct {
 	ID           int    `json:"ID"`
 	Source       string `json:"Source" binding:"required"`
-	Destionation string `jscon:"Destionation" binding:"required"`
+	Destionation string `json:"Destionation" binding:"required"`
 }
